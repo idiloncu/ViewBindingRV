a4503be391724a6275f014a9be1fa0716ddf0f36
octopus-idil <oncuidil@gmail.com>
2024-10-03 17:19:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,10 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Title 1", "13:04"),
+        Task("Title 2", "13:34"),
+        Task("Title 3", "13:44"),
+        Task("Title 4", "13:54"),
+    )
+}
